b3945c62c4ce79dea5f2b594a56852aa20a7b04f
wesbdss <wesleybenicio4@gmail.com>
2020-05-05 14:00:51 -0300

    a

--- teste3/index.js
+++ teste3/index.js
@@ -5,9 +5,9 @@ var a = document.getElementById('ace-x');
 function  (event) {
   var acceleration = event.acceleration;
   var interval = event.interval;
-  $('ace-x').html(acceleration.x.toFixed(3));
-  $('ace-y').html(acceleration.y.toFixed(3));
-  $('ace-z').html(acceleration.z.toFixed(3));
-  $('inter').html(interval);
+  $('#ace-x').html(acceleration.x.toFixed(3));
+  $('#ace-y').html(acceleration.y.toFixed(3));
+  $('#ace-z').html(acceleration.z.toFixed(3));
+  $('#inter').html(interval);
   a.innerHTML = acceleration.x.toFixed(3);
 }

94ca538ae47d90f8b255020860d7a95c28331a21
wesbdss <wesleybenicio4@gmail.com>
2020-05-05 15:01:05 -0300

    Adicionando movimentos do sensor

--- teste3/index.js
+++ teste3/index.js
@@ -1,10 +1,14 @@
 //sensor detect
 window.addEventListener("devicemotion", handleMotion, true);
 var a = document.getElementById('ace-x');
-$('#ace-x').html("null");
-$('#ace-y').html("null");
-$('#ace-z').html("null");
-$('#inter').html("null");
+$('#ace-x').html(0);
+$('#ace-y').html(0);
+$('#ace-z').html(0);
+$('#inter').html(0);
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 function handleMotion(event) {
     var acceleration = event.acceleration;
     var interval = event.interval;
@@ -12,30 +16,34 @@ function handleMotion(event) {
     $('#ace-y').html(acceleration.y.toFixed(3));
     $('#ace-z').html(acceleration.z.toFixed(3));
     $('#inter').html(interval);
-    a.innerHTML = acceleration.x.toFixed(3);
+    sleep(100);
+    
 }
 
 
 function main() {
-    const canvas = $("#canvas");
-    const renderer = new THREE.WebGLRenderer({ canvas });
+    const my_canvas = document.getElementById('canvas');
+    const renderer = new THREE.WebGLRenderer({ canvas:my_canvas });
     var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var camera = new THREE.PerspectiveCamera(75, my_canvas.offsetWidth / my_canvas.offsetHeight, 0.1, 1000);
+    camera.position.z=50;
 
     var geometry = new THREE.BoxGeometry(20, 20, 20);
     var material = new THREE.MeshLambertMaterial({ color: 0xfd59d7 });
     var cube = new THREE.Mesh(geometry, material);
 
+    scene.add(cube);
+    
     var light = new THREE.PointLight(0xFFFF00);
     light.position.set(10, 0, 25);
 
     scene.add(light);
-    scene.add(cube);
+    
     var render = function () {
         requestAnimationFrame(render);
 
-        cube.rotation.x += 0.1;
-        cube.rotation.y += 0.1;
+        cube.rotation.x += parseInt($("#ace-x").html());
+        cube.rotation.y += parseInt($("#ace-y").html());
         camera.updateProjectionMatrix();
 
         renderer.render(scene, camera);
